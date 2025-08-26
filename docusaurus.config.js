234ad8521a6491a6ace47919b475bbee6cb9c05d
Eureka <2560291241@qq.com>
2025-08-26 17:33:23 +0800

    1、现创建新分支git-pages，这个分支主要用于将网站部署到git pages上；尽量不要将这个分支和main分支合并，因为设置会被覆盖。如果已经合并，后续注意要修改，docusaurus.config.js和docsearch.json这两个文件，把其中的连接和Algolia搜索的Index名称给改到对应的；

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://eurekax.space/',
+  url: 'https://eureka12138github.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -88,7 +88,7 @@ const config = {
 		appId: '7Z8F108UT0',
 		//  Search-Only API Key
 		apiKey: '51c5befc0dffa0fbf8e0066c16535280',
-		indexName: 'Eureka-X-Space',
+		indexName: 'GitPages-Index',
 		searchPagePath: 'search',
 		contextualSearch: true
       },		
